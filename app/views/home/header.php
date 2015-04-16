0ef5383e6dc97b0394a9abd7ba162b2b9fe6afa0
Max Lopez <lopezmax.p@gmail.com>
2015-04-16 16:49:34 -0500

    Enlace home

--- app/views/home/header.php
+++ app/views/home/header.php
@@ -2,9 +2,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-5 col-md-4 col-lg-3">
-				<div id="main-logo"><a href="#"><?=HTML::image('img/logo.jpg')?></a></div>
+				<div id="main-logo"><a href="http://www.helloworldperu.org"></a></div>
 			</div>
-			<div class="col-xs-3 col-sm-3 col-sm-push-1 col-md-2 col-md-push-2 ">
+			<div class="col-xs-3 col-sm-3 col-sm-push-1 col-md-2 col-md-push-2">
 				<div class="helloworld"></div>
 			</div>
 			<div class="col-xs-3 col-xs-push-1 col-sm-3 col-sm-push-2 col-md-2 col-md-push-5 col-lg-2 col-lg-push-6">
