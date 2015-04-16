b9ed0630b1fee24fdb969756d5af85d8cd32031b
Adrian Ruiz <aerf2007@gmail.com>
2015-04-16 15:00:17 -0500

    Más cambios en el Header

--- app/views/home/header.php
+++ app/views/home/header.php
@@ -7,7 +7,7 @@
 			<div class="col-xs-3 col-sm-3 col-sm-push-1 col-md-2 col-md-push-2 ">
 				<div class="helloworld"></div>
 			</div>
-			<div class="col-xs-3 col-xs-push-1 col-sm-3 col-sm-push-3 col-md-2 col-md-push-5 col-lg-2 col-lg-push-6">
+			<div class="col-xs-3 col-xs-push-1 col-sm-3 col-sm-push-2 col-md-2 col-md-push-5 col-lg-2 col-lg-push-6">
 				<div class="ausp-menu"></div>
 			</div>
 		</div>
