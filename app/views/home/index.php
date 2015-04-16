e16daa11c263bc6fff117649de1dc6e5854832ef
Adrian Ruiz <aerf2007@gmail.com>
2015-04-15 22:44:21 -0500

    Autofocus Reparado

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -122,7 +122,7 @@
 						<div class="form-group">
 			                <div class="input-group">
 			                    <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-			                    <input type="text" placeholder="Nombres" name="nombres" required autofocus class="form-control">
+			                    <input type="text" placeholder="Nombres" name="nombres" required <?php if($mensaje)echo 'autofocus'; ?> class="form-control">
 			                </div>
 			            </div>
 			            <div class="form-group">
